bd9d28e45e07e5a0796cd0e5a8fbc4ced6eaa5df
PismoAI <pismogithub@gmail.com>
2026-01-01 18:32:33 -0800

    Re-enable term native build to fix crash

--- term/src/main/jni/Android.mk
+++ term/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := androidaicli-native
+LOCAL_MODULE    := jackpal-androidterm5
 LOCAL_SRC_FILES := common.cpp fileCompat.cpp termExec.cpp
 LOCAL_LDLIBS    := -llog
 
